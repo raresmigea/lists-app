c1eb745b19a76d9f719330270c498e2913a5ee12
raresmigea <raresini@yahoo.com>
2020-05-22 22:15:08 +0300

    show data in table

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 
 class Table extends Component {
   render() {
-    return <div>Table</div>;
+    const rows = [];
+    let lastCategory = null;
+    this.props.products.forEach((product) => {
+      if (lastCategory !== product.category) {
+        rows.push(product.category.toUpperCase());
+      }
+      rows.push(<div>{product.name + ' ' + product.price}</div>);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
   }
 }
 
